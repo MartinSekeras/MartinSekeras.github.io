ee86eb7fdf211cf0d2dcc23d16496bb5fcac8c41
Martin Sekeres <46966349+MartinSekeras@users.noreply.github.com>
2023-06-03 19:45:32 +0100

    Updated function
    Updated function to check both vertical and horizontal placement for each calculation.

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
 function calculateMaxProducts() {
+  var palletLength = parseInt(document.getElementById("palletLength").value);
+  var palletWidth = parseInt(document.getElementById("palletWidth").value);
+  var palletHeight = parseInt(document.getElementById("palletHeight").value);
+  var maxPalletWeight = parseInt(document.getElementById("maxPalletWeight").value);
+  
+  var productLength = parseInt(document.getElementById("productLength").value);
+  var productWidth = parseInt(document.getElementById("productWidth").value);
+  var productHeight = parseInt(document.getElementById("productHeight").value);
+  var productWeight = parseInt(document.getElementById("productWeight").value);
+  
+  var maxProductsLengthwise = Math.floor(palletLength / productLength) * Math.floor(palletWidth / productWidth) * Math.floor(palletHeight / productHeight);
+  var maxProductsSideways = Math.floor(palletLength / productWidth) * Math.floor(palletWidth / productLength) * Math.floor(palletHeight / productHeight);
+  var maxProductsHeightwise = Math.floor(palletLength / productHeight) * Math.floor(palletWidth / productWidth) * Math.floor(palletHeight / productLength);
+  
+  var maxProducts = Math.max(maxProductsLengthwise, maxProductsSideways, maxProductsHeightwise);
+  
+  var orientation = "";
+  if (maxProducts == maxProductsLengthwise) {
+    orientation = "Lengthwise";
+  } else if (maxProducts == maxProductsSideways) {
+    orientation = "Sideways";
+  } else {
+    orientation = "Heightwise";
+  }
+  
+  var maxWeight = maxProducts * productWeight;
+  if (maxWeight > maxPalletWeight) {
+    maxProducts = Math.floor(maxPalletWeight / productWeight);
+  }
+  
+  // Additional product placement logic
+  var maxProductsDoublePallet = 0;
+  if (productLength <= (palletLength / 2) && productWidth <= palletWidth && productHeight <= palletHeight) {
+    var maxProductsFlat = Math.floor(palletLength / productLength) * Math.floor(palletWidth / productWidth) * Math.floor(palletHeight / productHeight);
+    var maxProductsStanding = Math.floor((palletWidth - productWidth) / productLength) * Math.floor(palletLength / productWidth) * Math.floor(palletHeight / productHeight);
+    maxProductsDoublePallet = Math.max(maxProductsFlat, maxProductsStanding);
+  }
+  
+  if (maxProductsDoublePallet > maxProducts) {
+    maxProducts = maxProductsDoublePallet;
+    orientation = "Double Pallet";
+  }
+  
+  document.getElementById("maxProducts").innerHTML = "Maximum number of products that can be stacked: " + maxProducts;
+  document.getElementById("optimalOrientation").innerHTML = "Optimal orientation: " + orientation;
+}
+
+
+/*
+function calculateMaxProducts() {
     var palletLength = parseInt(document.getElementById("palletLength").value);
     var palletWidth = parseInt(document.getElementById("palletWidth").value);
     var palletHeight = parseInt(document.getElementById("palletHeight").value);
@@ -44,5 +94,4 @@ function calculateMaxProducts() {
     document.getElementById("productWeight").value = "";
     document.getElementById("maxProducts").innerHTML = "";
     document.getElementById("optimalOrientation").innerHTML = "";
-  }
-  
+  }*/
