48173db9b35061a3439330f1d7656b66d7f8edd5
Martin Sekeres <46966349+MartinSekeras@users.noreply.github.com>
2023-06-06 15:33:48 +0100

    Added layered calculation based on physical dimensions and volume.

--- script.js
+++ script.js
@@ -45,6 +45,128 @@ function handleCheckboxChange(checkbox) {
 function calculateMaxProducts() {
   var singlePalletCheckbox = document.getElementById("singlePalletCheckbox");
   var doublePalletCheckbox = document.getElementById("doublePalletCheckbox");
+  var customPalletCheckbox = document.getElementById("customPalletCheckbox");
+  
+  // Single Pallet
+  if (singlePalletCheckbox.checked) {
+    var palletLength = parseInt(document.getElementById("singlePalletLength").value);
+    var palletWidth = parseInt(document.getElementById("singlePalletWidth").value);
+    var palletHeight = parseInt(document.getElementById("singlePalletHeight").value);
+    var maxPalletWeight = parseInt(document.getElementById("singlePalletMaxWeight").value);
+  } 
+  // Double Pallet
+  else if (doublePalletCheckbox.checked) {
+    var palletLength = parseInt(document.getElementById("doublePalletLength").value);
+    var palletWidth = parseInt(document.getElementById("doublePalletWidth").value);
+    var palletHeight = parseInt(document.getElementById("doublePalletHeight").value);
+    var maxPalletWeight = parseInt(document.getElementById("doublePalletMaxWeight").value);
+  } 
+  // No Pallet checkbox is selected
+  else {
+    alert("Please select a pallet configuration.");
+    return;
+  }
+  
+  var productLength = parseInt(document.getElementById("productLength").value);
+  var productWidth = parseInt(document.getElementById("productWidth").value);
+  var productHeight = parseInt(document.getElementById("productHeight").value);
+  var productWeight = parseInt(document.getElementById("productWeight").value);
+  
+  // Validate input values
+  if (isNaN(palletLength) || isNaN(palletWidth) || isNaN(palletHeight) || isNaN(maxPalletWeight) || isNaN(productLength) || isNaN(productWidth) || isNaN(productHeight) || isNaN(productWeight)) {
+    alert("Please enter valid product dimensions.");
+    return;
+  }
+  
+  var maxProducts = 0;
+  var orientation = "";
+  
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      for (var k = 0; k < 3; k++) {
+        var orientationLengthwise = Math.floor(palletLength / productLength) * Math.floor(palletWidth / productWidth) * Math.floor(palletHeight / productHeight);
+        var orientationSideways = Math.floor(palletLength / productWidth) * Math.floor(palletWidth / productLength) * Math.floor(palletHeight / productHeight);
+        var orientationHeightwise = Math.floor(palletLength / productHeight) * Math.floor(palletWidth / productWidth) * Math.floor(palletHeight / productLength);
+  
+        var maxProductsOrientation = Math.max(orientationLengthwise, orientationSideways, orientationHeightwise);
+  
+        if (maxProductsOrientation > maxProducts) {
+          maxProducts = maxProductsOrientation;
+  
+          if (maxProducts === orientationLengthwise) {
+            orientation = "Lengthwise";
+          } else if (maxProducts === orientationSideways) {
+            orientation = "Sideways";
+          } else {
+            orientation = "Heightwise";
+          }
+        }
+  
+        // Rotate the product dimensions for the next iteration
+        var temp = productLength;
+        productLength = productWidth;
+        productWidth = productHeight;
+        productHeight = temp;
+      }
+  
+      // Rotate the product dimensions for the next iteration
+      var temp = productLength;
+      productLength = productHeight;
+      productHeight = productWidth;
+      productWidth = temp;
+    }
+  
+    // Rotate the product dimensions for the next iteration
+    var temp = productHeight;
+    productHeight = productWidth;
+    productWidth = productLength;
+    productLength = temp;
+  }
+  
+  var maxWeight = maxProducts * productWeight;
+  if (maxWeight > maxPalletWeight) {
+    maxProducts = Math.floor(maxPalletWeight / productWeight);
+    orientation = "";
+  }
+  
+  var maxProductsDoublePallet = 0;
+  if (productLength <= palletLength / 2 && productWidth <= palletWidth && productHeight <= palletHeight) {
+    var maxProductsFlat = Math.floor(palletLength / productLength) * Math.floor(palletWidth / productWidth) * Math.floor(palletHeight / productHeight);
+    var maxProductsStanding = Math.floor((palletWidth - productWidth) / productLength) * Math.floor(palletLength / productWidth) * Math.floor(palletHeight / productHeight);
+    maxProductsDoublePallet = Math.max(maxProductsFlat, maxProductsStanding);
+  }
+  
+  if (maxProductsDoublePallet > maxProducts) {
+    maxProducts = maxProductsDoublePallet;
+    orientation = "";
+  }
+  
+  var warningMessageOne = document.getElementById("warningMessageOne");
+  var warningMessageTwo = document.getElementById("warningMessageTwo");
+  
+  if (palletLength <= 1200 && maxPalletWeight > 601) {
+    warningMessageOne.innerHTML = "Warning: Pallet may be overweight for racking, (>600kg).";
+    warningMessageTwo.innerHTML = "Please select a pallet weight of 500kg max for racking.";
+  } else if (palletLength > 1200 && palletLength <= 2400 && maxPalletWeight > 501) {
+    warningMessageOne.innerHTML = "Warning: This pallet may be overweight for racking, (>500kg).";
+    warningMessageTwo.innerHTML = "For pallets of this size, consider setting the max weight as 500kg for racking.";
+  } else if (maxPalletWeight > 850) {
+    warningMessageOne.innerHTML = "Warning: Maximum permissible weight of the pallet is 850kg.";
+    warningMessageTwo.innerHTML = "";
+  } else {
+    warningMessageOne.innerHTML = "";
+    warningMessageTwo.innerHTML = "";
+  }
+  
+  document.getElementById("maxProducts").innerHTML = "Maximum number of products that can be stacked: " + maxProducts;
+  document.getElementById("optimalOrientation").innerHTML = "Optimal orientation: " + orientation;
+}
+
+
+/*
+function calculateMaxProducts() {
+  var singlePalletCheckbox = document.getElementById("singlePalletCheckbox");
+  var doublePalletCheckbox = document.getElementById("doublePalletCheckbox");
   var customPalletCheckbox = document.getElementById("doublePalletCheckbox");
   
   // Single Pallet
@@ -130,3 +252,4 @@ function calculateMaxProducts() {
   document.getElementById("maxProducts").innerHTML = "Maximum number of products that can be stacked: " + maxProducts;
   document.getElementById("optimalOrientation").innerHTML = "Optimal orientation: " + orientation;
 }
+*/
